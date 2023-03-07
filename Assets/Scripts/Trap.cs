835cdbed2889813d337a2b387468e1ffa807defb
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-03-07 20:08:36 +0200

    Disable animation after collision

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -3,14 +3,19 @@ using UnityEngine.Events;
 
 public class Trap : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
     public event UnityAction Destroying;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent<Cubic>(out Cubic cubic))
         {
-            if(cubic.CanDestroy)
+            if (cubic.CanDestroy)
+            {
                 Destroying.Invoke();
+                _animator.enabled = false;
+            }
+
         }
     }
 }
