65bebdc5f7fd0e76b03124b2305f37ab68d1ea1b
Crackbite <94061798+Crackbite@users.noreply.github.com>
2023-03-17 19:44:26 +0500

    Add the Bounds public property to PressStand

--- Assets/Scripts/HydraulicPress/PressStand.cs
+++ Assets/Scripts/HydraulicPress/PressStand.cs
@@ -1,5 +1,14 @@
 using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class PressStand : MonoBehaviour
 {
+    private MeshRenderer _meshRenderer;
+
+    public Bounds Bounds => _meshRenderer.bounds;
+
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
 }
