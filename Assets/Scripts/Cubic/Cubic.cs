8c3659c63f931d6720fbce58095661049863da59
Crackbite <94061798+Crackbite@users.noreply.github.com>
2023-03-17 19:44:01 +0500

    Add the Bounds public property to Cubic

--- Assets/Scripts/Cubic/Cubic.cs
+++ Assets/Scripts/Cubic/Cubic.cs
@@ -1,11 +1,20 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class Cubic : MonoBehaviour
 {
+    private MeshRenderer _meshRenderer;
     private bool _steppedOnStand;
 
     public event Action<PressStand> SteppedOnStand;
+
+    public Bounds Bounds => _meshRenderer.bounds;
+
+    private void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
 
     private void OnTriggerEnter(Collider collision)
     {
