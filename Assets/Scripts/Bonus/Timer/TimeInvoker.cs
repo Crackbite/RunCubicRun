c7b29558619166d62ce59ae4527d354e788c312d
Crackbite <94061798+Crackbite@users.noreply.github.com>
2023-04-09 17:08:22 +0500

    Create a timer to work with bonuses

---
+++ Assets/Scripts/Bonus/Timer/TimeInvoker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class TimeInvoker : MonoBehaviour
+{
+    private static TimeInvoker instance;
+
+    private float _oneSecTimer;
+
+    public event Action OnOneSyncedSecondTickedEvent;
+
+    public static TimeInvoker Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                var go = new GameObject("[TIME INVOKER]");
+                instance = go.AddComponent<TimeInvoker>();
+                DontDestroyOnLoad(go);
+            }
+
+            return instance;
+        }
+    }
+
+    private void Update()
+    {
+        float deltaTimer = Time.deltaTime;
+
+        _oneSecTimer += deltaTimer;
+
+        if (_oneSecTimer >= 1f)
+        {
+            _oneSecTimer -= 1f;
+
+            OnOneSyncedSecondTickedEvent?.Invoke();
+        }
+    }
+}
