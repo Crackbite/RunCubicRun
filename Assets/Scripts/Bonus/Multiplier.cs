107e5613864c63a462292d87b2a0f9897416c0d7
Crackbite <94061798+Crackbite@users.noreply.github.com>
2023-04-09 17:45:39 +0500

    Implement the Multiplier bonus

---
+++ Assets/Scripts/Bonus/Multiplier.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Multiplier : Bonus
+{
+    [SerializeField] private float _scoreMultiplier = 2f;
+    [SerializeField] private ScoreAllocator _scoreAllocator;
+
+    private float _initialScoreMultiplier;
+
+    public override void Apply()
+    {
+        _initialScoreMultiplier = _scoreAllocator.ScoreMultiplier;
+        _scoreAllocator.ScoreMultiplier = _scoreMultiplier;
+    }
+
+    public override void Cancel()
+    {
+        _scoreAllocator.ScoreMultiplier = _initialScoreMultiplier;
+    }
+}
