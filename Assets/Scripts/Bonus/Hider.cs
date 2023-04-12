87f40cc803fc6233a75222a3e6de880746ce2489
Crackbite <94061798+Crackbite@users.noreply.github.com>
2023-04-13 01:38:06 +0500

    Implement the Hider bonus

---
+++ Assets/Scripts/Bonus/Hider.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hider : Bonus
+{
+    [SerializeField] private Transform _roadsContainer;
+    [SerializeField] private Transform _portalsContainer;
+    [SerializeField] private LayerMask _hiddenLayer = 1 << 9;
+    [SerializeField] private BlockStackRenderer _blockStackRenderer;
+
+    private Dictionary<ColorBlock, int> _modifiedBlocks;
+    private Dictionary<Portal, Color> _modifiedPortals;
+
+    public override void Apply()
+    {
+        HideUnnecessaryBlocks();
+        UpdatePortalColors();
+    }
+
+    public override void Cancel()
+    {
+        ShowAllBlocks();
+        RestorePortalColors();
+    }
+
+    private void HideUnnecessaryBlocks()
+    {
+        int newLayer = (int)Mathf.Log(_hiddenLayer.value, 2);
+
+        ColorBlock[] colorBlocks = _roadsContainer.GetComponentsInChildren<ColorBlock>();
+        _modifiedBlocks = new Dictionary<ColorBlock, int>(colorBlocks.Length);
+
+        foreach (ColorBlock colorBlock in colorBlocks)
+        {
+            if (colorBlock.BlockRenderer.CurrentColor == _blockStackRenderer.CurrentColor)
+            {
+                continue;
+            }
+
+            _modifiedBlocks.Add(colorBlock, colorBlock.gameObject.layer);
+
+            colorBlock.gameObject.layer = newLayer;
+            colorBlock.BlockPhysics.TurnOffTrigger();
+        }
+    }
+
+    private void RestorePortalColors()
+    {
+        foreach ((Portal portal, Color originalColor) in _modifiedPortals)
+        {
+            portal.SetColor(originalColor);
+        }
+    }
+
+    private void ShowAllBlocks()
+    {
+        foreach ((ColorBlock colorBlock, int originalLayer) in _modifiedBlocks)
+        {
+            colorBlock.gameObject.layer = originalLayer;
+            colorBlock.BlockPhysics.TurnOnTrigger();
+        }
+    }
+
+    private void UpdatePortalColors()
+    {
+        Portal[] portals = _portalsContainer.GetComponentsInChildren<Portal>();
+        _modifiedPortals = new Dictionary<Portal, Color>(portals.Length);
+
+        foreach (Portal portal in portals)
+        {
+            if (portal.Color == _blockStackRenderer.CurrentColor)
+            {
+                continue;
+            }
+
+            _modifiedPortals.Add(portal, portal.Color);
+            portal.SetColor(_blockStackRenderer.CurrentColor);
+        }
+    }
+}
--- Assets/Scripts/ColorBlock/ColorBlockPhysics.cs
+++ Assets/Scripts/ColorBlock/ColorBlockPhysics.cs
@@ -48,4 +48,9 @@ public class ColorBlockPhysics : MonoBehaviour
     {
         _collider.isTrigger = false;
     }
+
+    public void TurnOnTrigger()
+    {
+        _collider.isTrigger = true;
+    }
 }
