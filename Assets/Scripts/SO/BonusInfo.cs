7838bf17d05cc3089d0e5375cf73bbe29dd92d3b
Crackbite <94061798+Crackbite@users.noreply.github.com>
2023-04-25 00:26:10 +0500

    Add positive flag to bonus information

--- Assets/Scripts/SO/BonusInfo.cs
+++ Assets/Scripts/SO/BonusInfo.cs
@@ -6,7 +6,8 @@ public class BonusInfo : ScriptableObject
     [SerializeField] private Sprite _icon;
     [SerializeField] private string _description;
     [SerializeField] private float _duration;
-
+    [SerializeField] private bool _isPositive;
+    
     public string Description => _description;
     public float Duration => _duration;
     public Sprite Icon => _icon;
