456679ad53637be9f2f4270955696406754a0c3d
Crackbite <94061798+Crackbite@users.noreply.github.com>
2023-04-27 17:47:11 +0500

    Add containers for portals and colored blocks

---
+++ Assets/Scripts/Road/PortalsContainer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalsContainer : MonoBehaviour
+{
+    [SerializeField] private ChunkGenerator _chunkGenerator;
+
+    private Portal[] _portals;
+
+    public IReadOnlyList<Portal> Portals => _portals;
+
+    private void OnEnable()
+    {
+        _chunkGenerator.Completed += OnChunkGeneratorCompleted;
+    }
+
+    private void OnDisable()
+    {
+        _chunkGenerator.Completed -= OnChunkGeneratorCompleted;
+    }
+
+    private void OnChunkGeneratorCompleted()
+    {
+        _portals = FindObjectsOfType<Portal>();
+    }
+}
