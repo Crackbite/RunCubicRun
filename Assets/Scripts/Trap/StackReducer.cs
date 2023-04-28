1be74e25f8e9c6af982cf182c25c9448c8db0831
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-04-28 21:49:32 +0300

    Remove side setting from StackReducer

--- Assets/Scripts/Trap/StackReducer.cs
+++ Assets/Scripts/Trap/StackReducer.cs
@@ -7,16 +7,7 @@ public class StackReducer : MonoBehaviour
     [SerializeField] private GameObject _longCrossbar;
     [SerializeField] private GameObject _shortCrossbar;
     [SerializeField] private float _height;
-    [SerializeField] private float _rightPositionZ;
-    [SerializeField] private float _leftPositionZ;
     [SerializeField] private WidthType _width;
-    [SerializeField] private SideType _side;
-
-    public enum SideType
-    {
-        Left,
-        Right
-    }
 
     public enum WidthType
     {
@@ -26,10 +17,10 @@ public class StackReducer : MonoBehaviour
 
     private void Start()
     {
-        SetSize(_height, _width, _side);
+        SetSize(_height, _width);
     }
 
-    public void SetSize(float height, WidthType width, SideType side)
+    public void SetSize(float height, WidthType width)
     {
         Vector3 size = _pillar.localScale;
         _pillar.localScale = new Vector3(size.x, height, size.z);
@@ -46,19 +37,5 @@ public class StackReducer : MonoBehaviour
         {
             _shortCrossbar.SetActive(true);
         }
-
-        Vector3 position = transform.position;
-
-        if (side == SideType.Left)
-        {
-            position.z = _leftPositionZ;
-            _head.rotation = Quaternion.Euler(0, 180, 0);
-        }
-        else
-        {
-            position.z = _rightPositionZ;
-        }
-
-        transform.position = position;
     }
 }
