80dfaa6d66790ec58b112e4dc5d5cfc3cf658962
Crackbite <94061798+Crackbite@users.noreply.github.com>
2023-04-30 12:17:00 +0500

    Add characteristics to Chunk

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -2,4 +2,9 @@ using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
+    [SerializeField] private int _difficulty;
+    [SerializeField] private bool _canRotate;
+
+    public bool CanRotate => _canRotate;
+    public int Difficulty => _difficulty;
 }
