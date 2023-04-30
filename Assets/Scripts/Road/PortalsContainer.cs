31c4cdcbc00f934d94735b18366f9b4ba6ea35e1
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-04-30 10:20:59 +0300

    Add portal sorting to PortalsContainer

--- Assets/Scripts/Road/PortalsContainer.cs
+++ Assets/Scripts/Road/PortalsContainer.cs
@@ -22,5 +22,24 @@ public class PortalsContainer : MonoBehaviour
     private void OnChunkGeneratorCompleted()
     {
         _portals = FindObjectsOfType<Portal>();
+        SortPortalsByX();
+    }
+
+    private void SortPortalsByX()
+    {
+        for (int i = 1; i < _portals.Length; i++)
+        {
+            float keyX = _portals[i].transform.position.x;
+            Portal keyPortal = _portals[i];
+            int j = i - 1;
+
+            while (j >= 0 && _portals[j].transform.position.x > keyX)
+            {
+                _portals[j + 1] = _portals[j];
+                j--;
+            }
+
+            _portals[j + 1] = keyPortal;
+        }
     }
 }
