b73c3a505bc29d4c176faf4039843493036d7016
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-04-30 17:57:44 +0300

    Reverse rotation of SpinningSaws on chunk flip

--- Assets/Scripts/Trap/HorizontalSaw.cs
+++ Assets/Scripts/Trap/HorizontalSaw.cs
@@ -2,17 +2,47 @@ using UnityEngine;
 
 public class HorizontalSaw : Saw
 {
+    [SerializeField] private float _defaultSpeed = 1.4f;
     [SerializeField] private bool _canRandomizeSpeed;
+    [SerializeField] private SawSide _sawSide;
+
+    private enum SawSide { Left, Right }
 
     protected override void SetSpeed()
     {
         if (_canRandomizeSpeed)
         {
-            base.SetSpeed();
+            float speed = Random.Range(MinSpeed, MaxSpeed) * GetRotationDirection();
+            Animator.SetFloat(SpeedId, speed);
         }
         else
         {
+            Animator.SetFloat(SpeedId, _defaultSpeed * GetRotationDirection());
             return;
         }
+    }
+
+    private int GetRotationDirection()
+    {
+        const float CenterPositionZ = 0;
+        const int RightRotationValue = 1;
+        const int LeftRotationValue = -1;
+
+        if (_sawSide == SawSide.Right)
+        {
+            if(transform.position.z < CenterPositionZ)
+            {
+                return RightRotationValue;
+            }
+
+            return LeftRotationValue;
+        }
+
+        if (transform.position.z > CenterPositionZ)
+        {
+            return RightRotationValue;
+        }
+
+        return LeftRotationValue;
     }
 }
