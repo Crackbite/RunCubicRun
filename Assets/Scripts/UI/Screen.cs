eb2e59341de2cf0e881a2b48e8b81a8a337a0798
Crackbite <94061798+Crackbite@users.noreply.github.com>
2023-05-30 15:27:21 +0500

    Implement animation rewind before screen off

--- Assets/Scripts/UI/Screen.cs
+++ Assets/Scripts/UI/Screen.cs
@@ -1,7 +1,15 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 public abstract class Screen : MonoBehaviour
 {
+    private DOTweenAnimation[] _tweenAnimations;
+
+    private void Start()
+    {
+        _tweenAnimations = GetComponentsInChildren<DOTweenAnimation>();
+    }
+
     public virtual void Enter()
     {
         gameObject.SetActive(true);
@@ -9,6 +17,18 @@ public abstract class Screen : MonoBehaviour
 
     public virtual void Exit()
     {
-        gameObject.SetActive(false);
+        if (_tweenAnimations.Length > 0)
+        {
+            foreach (DOTweenAnimation doTweenAnimation in _tweenAnimations)
+            {
+                doTweenAnimation.DOPlayBackwards();
+            }
+
+            _tweenAnimations[0].tween.OnRewind(() => gameObject.SetActive(false));
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
