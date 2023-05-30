38d4ee022ee71f12e1031a03d17cc1c8290289a4
Crackbite <94061798+Crackbite@users.noreply.github.com>
2023-05-31 02:01:53 +0500

    Add disabling buttons when they are animated

--- Assets/Scripts/UI/Screen.cs
+++ Assets/Scripts/UI/Screen.cs
@@ -1,22 +1,35 @@
 ﻿using DG.Tweening;
 using UnityEngine;
 
+[RequireComponent(typeof(CanvasGroup))]
 public abstract class Screen : MonoBehaviour
 {
+    private CanvasGroup _canvasGroup;
     private DOTweenAnimation[] _tweenAnimations;
 
     private void Start()
     {
+        _canvasGroup = GetComponent<CanvasGroup>();
         _tweenAnimations = GetComponentsInChildren<DOTweenAnimation>();
     }
 
     public virtual void Enter()
     {
+        if (_canvasGroup != null)
+        {
+            _canvasGroup.interactable = true;
+        }
+
         gameObject.SetActive(true);
     }
 
     public virtual void Exit()
     {
+        if (_canvasGroup != null)
+        {
+            _canvasGroup.interactable = false;
+        }
+
         if (_tweenAnimations.Length > 0)
         {
             foreach (DOTweenAnimation doTweenAnimation in _tweenAnimations)
