b98e7ed9b3faf758fa6ceebaae569e7ff2ea3474
Crackbite <94061798+Crackbite@users.noreply.github.com>
2023-05-30 15:35:18 +0500

    Implement score reset animation after game start

--- Assets/Scripts/Score/ScoreDisplay.cs
+++ Assets/Scripts/Score/ScoreDisplay.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -5,28 +7,35 @@ using UnityEngine;
 public class ScoreDisplay : MonoBehaviour
 {
     [SerializeField] private ScoreAllocator _scoreAllocator;
+    [SerializeField] private GameStatusTracker _gameStatusTracker;
+    [SerializeField] private float _scoreResetSpeed = .5f;
     [SerializeField] private bool _isDebug;
+
+    private readonly CultureInfo _cultureInfo = new CultureInfo("ru-RU");
 
     private TMP_Text _score;
 
-    private string _scoreText;
-
     private void OnEnable()
     {
+        _gameStatusTracker.GameStarted += OnGameStarted;
         _scoreAllocator.ScoreChanged += OnScoreChanged;
     }
 
     private void Start()
     {
         _score = GetComponent<TMP_Text>();
-        _scoreText = _score.text;
-
-        SetScore(0f);
+        SetScore(781638f);
     }
 
     private void OnDisable()
     {
+        _gameStatusTracker.GameStarted -= OnGameStarted;
         _scoreAllocator.ScoreChanged -= OnScoreChanged;
+    }
+
+    private void OnGameStarted()
+    {
+        SetScoreWithAnimation(0f);
     }
 
     private void OnScoreChanged(Score score)
@@ -41,6 +50,14 @@ public class ScoreDisplay : MonoBehaviour
 
     private void SetScore(float score)
     {
-        _score.text = $"{_scoreText}{score}";
+        _score.text = score.ToString("# ##0", _cultureInfo);
+    }
+
+    private void SetScoreWithAnimation(float score)
+    {
+        int fromValue = int.Parse(_score.text.Replace(" ", string.Empty));
+        int endValue = int.Parse(score.ToString(_cultureInfo));
+
+        _score.DOCounter(fromValue, endValue, _scoreResetSpeed, true, _cultureInfo);
     }
 }
