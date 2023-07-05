1b68413fa21e29f6d201c2ae5055a5f3fa2da12c
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-07-05 14:16:34 +0300

    Add effect for collision with the crossbar

--- Assets/Scripts/Trap/Crossbar.cs
+++ Assets/Scripts/Trap/Crossbar.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class Crossbar : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _collisionEffect;
     private Collider _collider;
 
     public TrapType Type => TrapType.Crossbar;
@@ -14,9 +15,13 @@ public class Crossbar : MonoBehaviour
 
     private void OnTriggerEnter(Collider collision)
     {
+
         if (collision.TryGetComponent(out ColorBlock _))
         {
             _collider.isTrigger = false;
+            Vector3 contactPoint = _collider.ClosestPoint(collision.transform.position);
+            _collisionEffect.transform.position = contactPoint;
+            _collisionEffect.Play();
         }
     }
 }
