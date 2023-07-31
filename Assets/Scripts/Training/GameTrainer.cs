77b41b98be7803d97a81b3f2edc8e544467da43a
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-07-31 22:37:12 +0300

    Add pauses to training

--- Assets/Scripts/Training/GameTrainer.cs
+++ Assets/Scripts/Training/GameTrainer.cs
@@ -1,58 +1,61 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class GameTrainer : MonoBehaviour
 {
     [SerializeField] private LevelEntryPortal _levelEntryPortal;
     [SerializeField] private CheckpointContainer _checkpointContainer;
-    [SerializeField] private PistonPresser _pistonPresser;
-    [SerializeField] private CubicInputHandler _cubicInputHandler;
-    [SerializeField] private CubicMovement _cubicMovement;
     [SerializeField] private TrainingPhraseDisplay _phraseDisplay;
-    [SerializeField] private bool _hasPressTraining;
+    [SerializeField] private CubicInputHandler _cubicInputHandler;
 
     private int _nextPhraseNumber;
-    private bool _canStopGame;
+    private bool _isGamePaused;
 
-    public bool IsTrainingOver { get; private set; }
-
-    private void OnEnable()
+    protected virtual void OnEnable()
     {
         _levelEntryPortal.ThrownOut += OnCubicThrownOut;
-        _cubicMovement.CubicLeftPress += OnCubicLeftPress;
-        _pistonPresser.CubicReached += OnPressCubicReached;
-
-        if (_hasPressTraining)
-        {
-            _phraseDisplay.DisplayCompleted += OnDisplayCompleted;
-            _pistonPresser.StackReached += OnStackReached;
-            _cubicInputHandler.PressSpeedReduced += OnPressSpeedReduced;
-        }
+        _cubicInputHandler.PressSpeedReduced += OnPressSpeedReduced;
+        _cubicInputHandler.LineChanged += OnLineChanged;
+        _phraseDisplay.DisplayCompleted += OnDisplayCompleted;
     }
 
-    private void OnDisable()
+    protected virtual void OnDisable()
     {
         _levelEntryPortal.ThrownOut -= OnCubicThrownOut;
-        _cubicMovement.CubicLeftPress -= OnCubicLeftPress;
-        _pistonPresser.CubicReached -= OnPressCubicReached;
-
-        if (_hasPressTraining)
-        {
-            _phraseDisplay.DisplayCompleted -= OnDisplayCompleted;
-            _pistonPresser.StackReached -= OnStackReached;
-            _cubicInputHandler.PressSpeedReduced -= OnPressSpeedReduced;
-        }
+        _cubicInputHandler.PressSpeedReduced -= OnPressSpeedReduced;
+        _cubicInputHandler.LineChanged -= OnLineChanged;
+        _phraseDisplay.DisplayCompleted -= OnDisplayCompleted;
     }
 
-    private void StartTraining()
+    protected void StartTraining()
     {
         const float Delay = 0.5f;
 
         if (_phraseDisplay.PhrasesAmount > _nextPhraseNumber)
         {
-            _phraseDisplay.CleanText();
             Invoke(nameof(Train), Delay);
         }
     }
+    protected void Unpause()
+    {
+        if (_isGamePaused)
+        {
+            Time.timeScale = 1f;
+            _phraseDisplay.CleanText();
+            _isGamePaused = false;
+        }
+    }
+
+    protected virtual void OnPressSpeedReduced()
+    {
+        Unpause();
+    }
+
+    protected virtual void OnLineChanged(Vector3 direction)
+    {
+        Unpause();
+    }
+
 
     private void Train()
     {
@@ -62,35 +65,10 @@ public class GameTrainer : MonoBehaviour
 
     private void OnDisplayCompleted()
     {
-        if (_canStopGame)
-        {
-            Time.timeScale = 0f;
-            _canStopGame = false;
-        }
+        Time.timeScale = 0f;
+        _isGamePaused = true;
     }
 
-    private void OnPressCubicReached(Cubic cubic)
-    {
-        _phraseDisplay.CleanText();
-    }
-
-    private void OnCubicLeftPress()
-    {
-        StartTraining();
-    }
-
-    private void OnPressSpeedReduced()
-    {
-        _cubicInputHandler.PressSpeedReduced -= OnPressSpeedReduced;
-        Time.timeScale = 1f;
-        StartTraining();
-    }
-
-    private void OnStackReached()
-    {
-        _canStopGame = true;
-        StartTraining();
-    }
 
     private void OnCubicThrownOut()
     {
