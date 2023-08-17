41384c0304a0cb2bb30505c4b8eb199c02d38464
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-08-17 21:46:56 +0300

    Implement music disable logic in settings

--- Assets/Scripts/Audio/Soundtrack.cs
+++ Assets/Scripts/Audio/Soundtrack.cs
@@ -6,12 +6,14 @@ public class Soundtrack : MonoBehaviour
     [SerializeField] private AudioClip _gameplayAudio;
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private GameStatusTracker _gameStatusTracker;
-    [SerializeField] private LevelEntryPortal _levelEntryPortal;
+    [SerializeField] private SwitchToggle _musicSwitchToggle;
+    [SerializeField] private SoundSystem _soundSystem;
 
     private void OnEnable()
     {
         _gameStatusTracker.GameStarted += OnGameStarted;
         _gameStatusTracker.GameEnded += OnGameEnded;
+        _musicSwitchToggle.ToggleChanged += OnMusicToggleChanged;
     }
 
     private void Awake()
@@ -23,6 +25,7 @@ public class Soundtrack : MonoBehaviour
     {
         _gameStatusTracker.GameStarted -= OnGameStarted;
         _gameStatusTracker.GameEnded -= OnGameEnded;
+        _musicSwitchToggle.ToggleChanged -= OnMusicToggleChanged;
     private void Play(AudioClip clip)
     {
         if (_isMusicOn)
@@ -35,12 +38,26 @@ public class Soundtrack : MonoBehaviour
 
     private void OnGameEnded(GameResult result)
     {
-        _gameplayAudio.Stop();
+        _audioSource.Stop();
     }
 
     private void OnGameStarted()
     {
         Play(_gameplayAudio);
     }
+
+    private void OnMusicToggleChanged(bool isSoundOn)
+    {
+        _soundSystem.Play(SoundEvent.SwitchToggle);
+        _isMusicOn = isSoundOn;
+
+        if (_isMusicOn == false)
+        {
+            _audioSource.Pause();
+        }
+        else if(_audioSource.isPlaying == false)
+        {
+            _audioSource.Play();
+        }
     }
 }
