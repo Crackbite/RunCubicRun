6367d468c18a2ef3f56d0f1a25d055f49eb9407b
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-08-28 22:31:50 +0300

    Rename PlayerPrefsKeys

---
+++ Assets/Scripts/SaveSystem/PlayerPrefsKeys.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+public static class PlayerPrefsKeys 
+{
+
+    public const string ScoreKey = nameof(ScoreKey);
+    public const string LeaderboardScoreKey = nameof(LeaderboardScoreKey);
+    public const string LevelKey = nameof(LevelKey);
+    public const string TrainingStageKey = nameof(TrainingStageKey);
+    public const string ActiveKey = nameof(ActiveKey);
+    public const string BoughtKey = nameof(BoughtKey);
+    public const string RestartKey = nameof(RestartKey);
+    public const string AuthKey = nameof(AuthKey);
+
+    static PlayerPrefsKeys()
+    {
+        Keys = new List<string>
+    {
+        ScoreKey,
+        LeaderboardScoreKey,
+        LevelKey,
+        TrainingStageKey,
+        ActiveKey,
+        BoughtKey,
+        RestartKey,
+        AuthKey,
+    };
+    }
+
+    public static IReadOnlyCollection<string> Keys { get; }
+
+}
