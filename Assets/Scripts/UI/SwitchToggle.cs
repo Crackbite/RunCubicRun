ef9559fd44eb93acd4b0867d16106a330ac8cfc6
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-09-07 23:42:22 +0300

    Remove Awake in SwitchToggle.cs

--- Assets/Scripts/UI/SwitchToggle.cs
+++ Assets/Scripts/UI/SwitchToggle.cs
@@ -8,7 +8,6 @@ public class SwitchToggle : MonoBehaviour
     [SerializeField] private float _animationDuration = 0.2f;
     [SerializeField] private RectTransform _switcherRectTransform;
     [SerializeField] private SettingsType _type;
-    [SerializeField] private DataRestorer _dataRestorer;
     [SerializeField] private Toggle _toggle;
 
     private Vector2 _handlePosition;
@@ -16,18 +15,6 @@ public class SwitchToggle : MonoBehaviour
     public event Action<bool, SwitchToggle> ToggleChanged;
 
     public SettingsType Type => _type;
-
-    private void Awake()
-    {
-        if (_type == SettingsType.Music)
-        {
-            ChangeHandlePosition(_dataRestorer.PlayerData.IsMusicOn);
-        }
-        else
-        {
-            ChangeHandlePosition(_dataRestorer.PlayerData.IsSoundOn);
-        }
-    }
 
     private void OnEnable()
     {
